a51c03bdf33ceeb658c2d6d4c87e429665904de8
koldointxausti <koldointxaustii@gmail.com>
2019-12-11 08:55:31 +0100

    hasta la fase 3

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts','BlogController@index')->name('posts.index');
+Route::get('/posts/create','BlogController@create')->name('posts.create');
+Route::get('/posts/{post}','BlogController@show')->name('posts.show');
+Route::get('/posts/{post}/edit','BlogController@edit')->name('posts.edit');
+Route::delete('/posts/{post}','BlogController@destroy')->name('posts.destroy');
